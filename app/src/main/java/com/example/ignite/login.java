bc3b822eae555b5049a8cabc42e4b5444030cc5b
adityasimant <adityasimant88@gmail.com>
2022-10-05 13:18:20 +0530

    initial

--- app/src/main/java/com/example/ignite/login.java
+++ app/src/main/java/com/example/ignite/login.java
@@ -5,13 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.QuickContactBadge;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class login extends AppCompatActivity {
 
     TextView signup;
     Button login;
+    EditText email,passwd;
 
 
     @Override
@@ -21,6 +24,9 @@ public class login extends AppCompatActivity {
 
         signup = findViewById(R.id.change_to_signup);
         login = findViewById(R.id.btn_login);
+        email = findViewById(R.id.edt_email);
+
+        passwd = findViewById(R.id.edt_password);
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +39,9 @@ public class login extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+
+
                 Intent intent = new Intent(login.this,MainActivity.class);
                 startActivity(intent);
 
