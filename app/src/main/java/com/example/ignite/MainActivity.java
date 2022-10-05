9d1ed18db8b32508ec3689736599b89838e9ca74
Ujair Shaha <107527307+UJAIR-SHAHA@users.noreply.github.com>
2022-10-06 00:14:38 +0530

    Updated MainActivity and added splash screen

--- app/src/main/java/com/example/ignite/MainActivity.java
+++ app/src/main/java/com/example/ignite/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.ignite;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView discover;
+    ImageView chat;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        discover =findViewById(R.id.uz_discover_logo);
+        chat = findViewById(R.id.uz_chat_logo);
+        chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,ChatActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        discover.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,DiscoverActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
