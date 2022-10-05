0bc31d50091c0b71c7448f9abdf87039449818f8
adityasimant <adityasimant88@gmail.com>
2022-10-05 08:21:52 +0530

    Modified signup & signin activities and wrote the logic for activity change.

--- app/src/main/java/com/example/ignite/login.java
+++ app/src/main/java/com/example/ignite/login.java
@@ -1,13 +1,32 @@
 package com.example.ignite;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.QuickContactBadge;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
+
+    TextView signup;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signup = findViewById(R.id.change_to_signup);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(login.this,sign_up.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/ignite/sign_up.java
+++ app/src/main/java/com/example/ignite/sign_up.java
@@ -1,13 +1,30 @@
 package com.example.ignite;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class sign_up extends AppCompatActivity {
+
+    TextView signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+
+        signin = findViewById(R.id.change_to_signin);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(sign_up.this,login.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
