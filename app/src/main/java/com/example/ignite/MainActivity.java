98662858cbebaa2fa8151e7ae7b2b748eebce441
adityasimant <adityasimant88@gmail.com>
2022-10-05 10:37:16 +0530

    Fixed issue of activity change

--- app/src/main/java/com/example/ignite/MainActivity.java
+++ app/src/main/java/com/example/ignite/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_main);
     }
 }
--- app/src/main/java/com/example/ignite/login.java
+++ app/src/main/java/com/example/ignite/login.java
@@ -4,13 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.QuickContactBadge;
 import android.widget.TextView;
 
 public class login extends AppCompatActivity {
 
     TextView signup;
-
+    Button login;
 
 
     @Override
@@ -19,6 +20,8 @@ public class login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         signup = findViewById(R.id.change_to_signup);
+        login = findViewById(R.id.btn_login);
+
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,6 +30,14 @@ public class login extends AppCompatActivity {
             }
         });
 
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(login.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
     }
 }
