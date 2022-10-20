dec3e190313c26447af57e3cb904b30c86e4adbc
adityasimant <adityasimant88@gmail.com>
2022-10-20 22:11:50 +0530

    test

--- app/src/main/java/Models/InterFace/OurRetrofitClient.java
+++ app/src/main/java/Models/InterFace/OurRetrofitClient.java
@@ -12,12 +12,10 @@ public interface OurRetrofitClient {
     @Headers({
             "Accept: application/json",
             "Content-Type: application/json",
-
+            "Authorization: Bearer pk_prod_R96PGPBAYY4J3EGBJPKB1NEG8MDE"
             })
 
     @POST("/send")
     Call <MainResponseModelClass> getPostValue(@Body MainObjectClass mainObjectClass);
-
-
 
 }
